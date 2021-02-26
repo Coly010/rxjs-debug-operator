f54f87565e207992042877ac3d0cfd1bd4e23299
Colum Ferry <cferry09@gmail.com>
2021-02-26 22:14:32 +0000

    feat(debug): add basic debug operator to log to console

--- packages/operators/debug/src/lib/operators-debug.spec.ts
+++ packages/operators/debug/src/lib/operators-debug.spec.ts
@@ -1,7 +1,18 @@
-import { operatorsDebug } from './operators-debug';
+import { of } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { debug } from './operators-debug';
 
-describe('operatorsDebug', () => {
-  it('should work', () => {
-    expect(operatorsDebug()).toEqual('operators-debug');
+describe('operators - debug', () => {
+  beforeEach(() => (console.log = jest.fn()));
+
+  it('should log value to the console', () => {
+    // ARRANGE
+    const obs$ = of('my test value');
+
+    // ACT
+    obs$.pipe(debug(), take(1)).subscribe();
+
+    // ASSERT
+    expect(console.log).toHaveBeenCalledWith('my test value');
   });
 });
--- packages/operators/debug/src/lib/operators-debug.ts
+++ packages/operators/debug/src/lib/operators-debug.ts
@@ -1,3 +1,7 @@
-export function operatorsDebug(): string {
-  return 'operators-debug';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { MonoTypeOperatorFunction } from 'rxjs/internal/types';
+
+export function debug<T>(): MonoTypeOperatorFunction<T> {
+  return (source: Observable<T>) => source.pipe(tap(console.log));
 }
